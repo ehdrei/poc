2703063005c72a7faffbde0f48de20cfdab91583
Ehdrei <ehdrei.aramburo@2bcore.com>
2018-03-14 11:21:05 -0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule} from '@angular/core';
 
 
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { AppComponent } from './app.component';
 
 import { environment } from '../environments/environment';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MenuComponent } from './menu/menu.component';
+import { HomeComponent } from './home/home.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MenuComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
+   // ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
+   environment.production ? ServiceWorkerModule.register('ngsw-worker.js') : []
   ],
   providers: [],
+  
   bootstrap: [AppComponent]
 })
 export class AppModule { }
